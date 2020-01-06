f8e59a21fa7b89627a34ac1e4aad7f6dd6c5a162
Beto Ortega <betoandriu01@gmail.com>
2020-01-06 09:33:53 -0500

    ademas del mensaje tambien se muestra el #

--- dec.asm
+++ dec.asm
@@ -2,11 +2,20 @@
         mov eax, 4
         mov ebx, 1
         push cx
+        add cx, '0'
+        mov [cont], cx
         mov ecx, %1
         mov edx, %2
         int 80h
+
+        mov eax, 4
+        mov ebx, 1
+        mov ecx, cont
+        mov edx, 1
+        int 80h
         pop cx
 %endmacro
+
 
 %macro leer 2 
         mov eax, 3
@@ -22,25 +31,25 @@ section .data
 section .bss
         n1 resb 2
         n2 resb 2
-        
+        cont resb 1
 section .text
         global _start 
 _start:
         
-        mov cx, 9
+        mov cx, 10
+        ;mov [cont], cx
 
 ciclo: 
         cmp cx, 0
         jz salir
         dec cx
-        
+        jnz imprimir_msj
 
 imprimir_msj:
         escribir msj, lenn1
 
         jmp ciclo
         
-
 
 salir: 
         mov eax, 1
